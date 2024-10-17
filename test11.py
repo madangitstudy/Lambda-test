e26653e55685640ad8a7e4ce55c97ab493dbfdc7
madangitstudy <109195282+madangitstudy@users.noreply.github.com>
2024-10-17 22:40:06 +0530

    Update test11.py

--- test11.py
+++ test11.py
@@ -1,60 +1,22 @@
+Test Event Name
+test1
+
+Response
 {
-    "data": {
-        "type": "event",
-        "attributes": {
-            "profile": {
-                "data": {
-                    "type": "profile",
-                    "attributes": {
-                        "email": "dylanreeveswasik@gmail.com"
-                    }
-                }
-            },
-            "metric": {
-                "data": {
-                    "type": "metric",
-                    "attributes": {
-                        "name": "OTPG voucher_email"
-                    }
-                }
-            },
-            "properties": {
-                "flight_details": {
-                    "actual_arrival_time": "2023-07-26T13:00:00",
-                    "arrival_airport_code": "LAX",
-                    "arrival_city": "Los Angeles",
-                    "departure_airport_code": "JFK",
-                    "departure_city": "New York",
-                    "expected_arrival_time": "2023-07-26T12:00:00",
-                    "flight_number": "AA101",
-                    "flight_status": "Delayed",
-                    "pnr_booking_id": "ABC123",
-                    "time_length_of_delay": 60
-                },
-                "passengers": [
-                    {
-                        "first_name": "John",
-                        "last_name": "Doe",
-                        "passenger_type": "adult",
-                        "voucher": {
-                            "expiry_date": "2023-07-31",
-                            "unique_voucher_code": "Voucher123",
-                            "voucher_value": 50
-                        }
-                    },
-                    {
-                        "first_name": "Jane",
-                        "last_name": "Doe",
-                        "passenger_type": "child",
-                        "voucher": {
-                            "expiry_date": null,
-                            "unique_voucher_code": null,
-                            "voucher_value": null,
-                            "message": "Passenger must be over 18 years old"
-                        }
-                    }
-                ]
-            }
-        }
-    }
+  "statusCode": 200,
+  "body": "{\"message\": \"Vouchers processed and updated.\"}"
 }
+
+Function Logs
+: "https://flairair-api.intelisystraining.ca/RESTv1/vouchers/4Ny63MjEXGYY85HIOcHapZiUxfvDBjeUXkAcWYmrJnY="}, {"href": "https://flairair-api.intelisystraining.ca/RESTv1/vouchers/4Ny63MjEXGYY85HIOcHapTa%C2%A5N7UOXSlE04swfoOu9zg="}, {"href": "https://flairair-api.intelisystraining.ca/RESTv1/vouchers/4Ny63MjEXGYY85HIOcHapW%C6%92rY9H5N9tMNmfPlp13WeY="}, {"href": "https://flairair-api.intelisystraining.ca/RESTv1/vouchers/4Ny63MjEXGYY85HIOcHapU%C2%A59RvP%C2%A5gf%C2%A5afeU6u4nIp7Y="}, {"href": "https://flairair-api.intelisystraining.ca/RESTv1/vouchers/4Ny63MjEXGYY85HIOcHapcO3La%C2%A5NIAK5DN87qvUMrz4="}, {"href": "https://flairair-api.intelisystraining.ca/RESTv1/vouchers/4Ny63MjEXGYY85HIOcHapc6KUcfQ18wXIZhUc4qyCb4="}, {"href": "https://flairair-api.intelisystraining.ca/RESTv1/vouchers/4Ny63MjEXGYY85HIOcHapXMhVMpWLM1pLkZ%C2%A5IcMydwE="}, {"href": "https://flairair-api.intelisystraining.ca/RESTv1/vouchers/4Ny63MjEXGYY85HIOcHapf8K%C2%A5iiVHr2YdF2SntALKOc="}, {"href": "https://flairair-api.intelisystraining.ca/RESTv1/vouchers/4Ny63MjEXGYY85HIOcHapXcW9FPPK%C6%92HvPXRf10EvqOw="}, {"href": "https://flairair-api.intelisystraining.ca/RESTv1/vouchers/4Ny63MjEXGYY85HIOcHapRibcA%C6%92ShOkKem9VUnmvuLM="}]
+API response data: [{"href": "https://flairair-api.intelisystraining.ca/RESTv1/vouchers/4Ny63MjEXGYY85HIOcHapQg7cBn0x7l1nHrgw0VE8ek="}, {"href": "https://flairair-api.intelisystraining.ca/RESTv1/vouchers/4Ny63MjEXGYY85HIOcHapSk2VagB%C2%A5BK%C2%A5WxveDYyU4r0="}, {"href": "https://flairair-api.intelisystraining.ca/RESTv1/vouchers/4Ny63MjEXGYY85HIOcHapUW4%C6%92vjVYNmTJ42NQqZt%C6%92MM="}, {"href": "https://flairair-api.intelisystraining.ca/RESTv1/vouchers/4Ny63MjEXGYY85HIOcHapYl%C2%A5waNiqhC8ecwaqcvssmA="}, {"href": "https://flairair-api.intelisystraining.ca/RESTv1/vouchers/4Ny63MjEXGYY85HIOcHapZiUxfvDBjeUXkAcWYmrJnY="}, {"href": "https://flairair-api.intelisystraining.ca/RESTv1/vouchers/4Ny63MjEXGYY85HIOcHapTa%C2%A5N7UOXSlE04swfoOu9zg="}, {"href": "https://flairair-api.intelisystraining.ca/RESTv1/vouchers/4Ny63MjEXGYY85HIOcHapW%C6%92rY9H5N9tMNmfPlp13WeY="}, {"href": "https://flairair-api.intelisystraining.ca/RESTv1/vouchers/4Ny63MjEXGYY85HIOcHapU%C2%A59RvP%C2%A5gf%C2%A5afeU6u4nIp7Y="}, {"href": "https://flairair-api.intelisystraining.ca/RESTv1/vouchers/4Ny63MjEXGYY85HIOcHapcO3La%C2%A5NIAK5DN87qvUMrz4="}, {"href": "https://flairair-api.intelisystraining.ca/RESTv1/vouchers/4Ny63MjEXGYY85HIOcHapc6KUcfQ18wXIZhUc4qyCb4="}, {"href": "https://flairair-api.intelisystraining.ca/RESTv1/vouchers/4Ny63MjEXGYY85HIOcHapXMhVMpWLM1pLkZ%C2%A5IcMydwE="}, {"href": "https://flairair-api.intelisystraining.ca/RESTv1/vouchers/4Ny63MjEXGYY85HIOcHapf8K%C2%A5iiVHr2YdF2SntALKOc="}, {"href": "https://flairair-api.intelisystraining.ca/RESTv1/vouchers/4Ny63MjEXGYY85HIOcHapXcW9FPPK%C6%92HvPXRf10EvqOw="}, {"href": "https://flairair-api.intelisystraining.ca/RESTv1/vouchers/4Ny63MjEXGYY85HIOcHapRibcA%C6%92ShOkKem9VUnmvuLM="}, {"href": "https://flairair-api.intelisystraining.ca/RESTv1/vouchers/4Ny63MjEXGYY85HIOcHapTfsj8KMOWMqKIgdiZkRTTc="}, {"href": "https://flairair-api.intelisystraining.ca/RESTv1/vouchers/4Ny63MjEXGYY85HIOcHapZQjvkBWhn4aW56kXhaocrs="}]
+Error updating voucher in DynamoDB: The provided key element does not match the schema
+Error updating voucher in DynamoDB: The provided key element does not match the schema
+Error updating voucher in DynamoDB: The provided key element does not match the schema
+Error updating voucher in DynamoDB: The provided key element does not match the schema
+Error updating voucher in DynamoDB: The provided key element does not match the schema
+Error updating voucher in DynamoDB: The provided key element does not match the schema
+Error updating voucher in DynamoDB: The provided key element does not match the schema
+Error updating voucher in DynamoDB: The provided key element does not match the schema
+END RequestId: 4f7f1402-7487-4031-b91d-ba6c971b444b
+REPORT RequestId: 4f7f1402-7487-4031-b91d-ba6c971b444b	Duration: 4451.46 ms	Billed Duration: 4452 ms	Memory Size: 128 MB	Max Memory Used: 80 MB	Init Duration: 299.11 ms
